5fd55abaaf2e4e23aa80d8ccdbb41bd421c8a6fe
Binhnn <binhnn@onaclover.com>
2019-06-11 19:13:32 +0700

    console: delete console.log

--- src/Redux/reducers/counterReducer/index.js
+++ src/Redux/reducers/counterReducer/index.js
@@ -7,13 +7,13 @@ const INITIAL_STATE = {
 }
 
 function counterReducer (state =  INITIAL_STATE, action) {
-  console.log(action, 'ah');
   switch (action.type) {
     case COUNTER.DECREMENT:
       return { ...state, total: action.step }
 
     case COUNTER.INCREMENT:
     return { ...state, total: action.step < 0 ? 0 : action.step }
+
     default:
       return state;
   }
